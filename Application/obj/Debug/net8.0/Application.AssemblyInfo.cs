11985de39fd2b4b7b6f9dca29a29509d784327ee
Saw Paing <sawp2489@gmail.com>
2024-10-04 17:09:11 -0500

    Completed Activity 5

--- Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3bf0a7e353192d62cbeb0aef26ad2d4678a09fb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8ed6ae7cc6771e5c67aa4f1509278b20ab59d47")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
